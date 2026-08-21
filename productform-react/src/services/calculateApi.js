1c0313c3de820c185eb5346d0869fe53111003a1
Haruki527 <fharuki527@gmail.com>
2026-08-22 02:18:24 +0900

    本番環境構築

--- productform-react/src/services/calculateApi.js
+++ productform-react/src/services/calculateApi.js
@@ -1,28 +1,20 @@
-// src/services/calculateApi.js
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-const API_URL = "http://localhost:8080/api/calculate";
-
-export async function calculateMoney(formData) {
-
-    // 空欄を0に変換
-    const requestData = {};
-
-    Object.keys(formData).forEach((key) => {
-        requestData[key] =
-            formData[key] === "" ? 0 : Number(formData[key]);
-    });
-
-    const response = await fetch(API_URL, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(requestData),
-    });
+export const calculateMoney = async (formData) => {
+    const response = await fetch(
+        `${API_BASE_URL}/api/calculate`,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(formData)
+        }
+    );
 
     if (!response.ok) {
-        throw new Error("計算に失敗しました。");
+        throw new Error("計算に失敗しました");
     }
 
     return await response.json();
-}
+};
